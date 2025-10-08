e4a565d797dfd63003fd44773fcd9061c6cbfbe2
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2025-10-08 10:22:02 -0500

    feat: Set up `build-logic` for convention plugins

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.raulastete.convention.buildlogic"
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.compose.gradlePlugin)
+}
+
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
+    }
+}
